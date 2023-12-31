1149b7a24529a686814b9a346187a9103a3d1892
Nika Ts <nika.tseleva@mail.ru>
2023-12-31 04:56:30 +0400

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import './index.css';
 import BottomBanner from "./components/BottomBanner";
 import TopBanner from "./components/TopBanner";
@@ -7,8 +6,12 @@ import TopBanner from "./components/TopBanner";
 function App() {
     return (
         <div className='relative w-full h-[3000px]' style={{fontFamily: 'GraphikLC'}}>
-            <div className={'relative top-[54px]'}><TopBanner/></div>
-            <div className={'fixed w-full sm:w-auto bottom-0 right-0 mx-0 md:mr-[17px] lg:mr-[34px]'}><BottomBanner/></div>
+            <div className={'relative top-[54px]'}>
+                <TopBanner/>
+            </div>
+            <div className={'fixed bottom-0 right-0 md:mr-[17px] lg:mr-[34px]'}>
+                <BottomBanner/>
+            </div>
         </div>
     );
 }
--- src/components/TopBanner/index.tsx
+++ src/components/TopBanner/index.tsx
@@ -28,9 +28,8 @@ const TopBanner = () => {
             </div>
             <div>
                 <div className={'flex flex-row items-center justify-center'}>
-                    <svg className={'block md:hidden ml-[18px] mr-6 cursor-pointer'} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.8373 22.8105C11.1302 23.1034 11.6051 23.1034 11.898 22.8105L19.1621 15.5463C19.2981 15.4104 19.3709 15.2352 19.3807 15.0573C19.4058 14.8378 19.3342 14.6094 19.1659 14.441L11.902 7.17716C11.6092 6.88427 11.1343 6.88427 10.8414 7.17717C10.5485 7.47006 10.5485 7.94493 10.8414 8.23783L17.5954 14.9918L10.8373 21.7498C10.5445 22.0427 10.5445 22.5176 10.8373 22.8105Z" fill="#D9D9D9" fill-opacity="0.4"/>
-                    </svg>
+                    {/*className={'block md:hidden ml-[18px] mr-6 cursor-pointer'}*/}
+
                     <button className={'hidden md:block w-[115px] h-[38px] rounded-[100px] bg-white cursor-pointer mr-6 lg:m-0'}>
                     <span
                         className={'text-[16px] leading-[17px] text-transparent bg-gradient-to-r from-black/60 to-30% to-black bg-clip-text'}>
